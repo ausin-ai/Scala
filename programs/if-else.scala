7e8f9d8e23ca79be08d6409445f50e7d45aa9137
Utkarsh Niraj Sinha <gitboyrex@gmail.com>
2024-08-03 23:12:54 +0530

    updated programs

---
+++ programs/if-else.scala
@@ -0,0 +1,12 @@
+object example {
+    def main(args: Array[String]): Unit = {
+        val age = 18
+
+        if (age >= 18){
+            println("you are an adult")
+        }
+        else {
+            println("you are teen")
+        }
+    }
+}
